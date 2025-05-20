62dcc146a8eacdc74616257b112d16ba353b09ad
BlaMacfly <blamacfly@github.com>
2025-05-20 20:35:10 +0200

    Nettoyage des fichiers inutilisés du site web Sylvania

--- test_access.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Script de test simple pour vérifier l'accès au serveur web
-echo "Le serveur web fonctionne correctement!";
-echo "<br>Date et heure: " . date('Y-m-d H:i:s');
-echo "<br>Serveur: " . $_SERVER['SERVER_NAME'];
-echo "<br>PHP version: " . phpversion();
-?>
